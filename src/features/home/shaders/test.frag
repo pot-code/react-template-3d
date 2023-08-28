a82b0198a75e8424a2ed565b3056ec210448835d
pot-code <136290354@qq.com>
2023-08-28 19:25:29 +0800

    feat: circles shader

--- src/features/home/shaders/test.frag
+++ src/features/home/shaders/test.frag
@@ -1,7 +1,13 @@
+uniform float uTime;
+
 in vec3 vPosition;
-in vec3 vNormal;
-in vec2 vUV;
 
 void main() {
-  gl_FragColor = vec4(vec3(step(0.5, vUV.x)), 1.);
+  // float speed = 2.0;
+  // float r = sin(uTime * speed + vPosition.x) * 0.3 + 0.7;
+  // float g = cos(uTime * speed + vPosition.y) * 0.3 + 0.7;
+  // float b = sin(uTime * speed + vPosition.z) * 0.3 + 0.7;
+  float gray = step(0.3, sin(length(50.0 * vPosition.xy)));
+
+  gl_FragColor = vec4(vec3(gray), 1.);
 }
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,18 +1,44 @@
 /* eslint-disable react/no-unknown-property */
 import { OrbitControls } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
-import { Color } from "three"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Color, ShaderMaterial } from "three"
+import { Pane } from "tweakpane"
 import vertexShader from "@/features/home/shaders/test.vert?raw"
 import fragmentShader from "@/features/home/shaders/test.frag?raw"
 
+function Plane() {
+  const mat = useRef<ShaderMaterial>(null!)
+
+  useFrame((state) => {
+    mat.current.uniforms.uTime.value = state.clock.getElapsedTime()
+  })
+
+  return (
+    <mesh>
+      <planeGeometry args={[2, 2]} />
+      <shaderMaterial
+        ref={mat}
+        uniforms={{ uTime: { value: 0 } }}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+      />
+    </mesh>
+  )
+}
+
 export default function HomeView() {
+  useEffect(() => {
+    const gui = new Pane()
+    return () => {
+      gui.dispose()
+    }
+  }, [])
+
   return (
     <div className="h-screen w-screen">
-      <Canvas scene={{ background: new Color(0xdddddd) }}>
-        <mesh>
-          <planeGeometry args={[2, 2]} />
-          <shaderMaterial vertexShader={vertexShader} fragmentShader={fragmentShader} />
-        </mesh>
+      <Canvas>
+        <Plane />
+        <color attach="background" args={[new Color(0xdddddd)]} />
         <OrbitControls />
       </Canvas>
     </div>
